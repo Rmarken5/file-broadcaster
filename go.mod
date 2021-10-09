b166fb27bdbbd1beeb2a562c187e1a993a4f469b
Ryan Marken <marken.ryan@gmail.com>
2021-10-09 11:32:34 -0400

    100% coverage for observer.go

--- go.mod
+++ go.mod
@@ -3,13 +3,8 @@ module github.com/Rmarken5/file-broadcaster
 go 1.16
 
 require (
-	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
-	github.com/derision-test/go-mockgen v1.1.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1
-	github.com/onsi/gomega v1.16.0 // indirect
-	golang.org/x/mod v0.5.1 // indirect
-	golang.org/x/net v0.0.0-20211007125505-59d4e928ea9d // indirect
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.7 // indirect
 )
--- observer/observer_test.go
+++ observer/observer_test.go
@@ -1,10 +1,55 @@
 package observer
 
 import (
+	"fmt"
+	mocks "github.com/Rmarken5/file-broadcaster/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"testing"
-	"github.com/Rmarken5/file-broadcaster/mocks"
 )
+//go:generate mockgen -destination=../mocks/mock_conn.go net Conn
+func TestConnectionData_OnUpdate_Nil(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-func TestConnectionData_OnUpdate(t *testing.T) {
-	conn := mocks.git
+	m := mocks.NewMockConn(ctrl)
+	m.EXPECT().Write(gomock.Any()).Return(11, nil)
+
+	conData := ConnectionData{
+		"addr",
+		m,
+	}
+
+	err := conData.OnUpdate([]string{"file-1.txt"})
+	if err != nil {
+		t.FailNow()
+	}
+}
+
+func TestConnectionData_OnUpdate_NotNil(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockConn(ctrl)
+	m.EXPECT().Write(gomock.Any()).Return(0, fmt.Errorf("SomeError"))
+
+	conData := ConnectionData{
+		"addr",
+		m,
+	}
+
+	err := conData.OnUpdate([]string{"file-1.txt"})
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestConnectionData_GetIdentifier(t *testing.T) {
+	connData := ConnectionData{
+		"hello",
+		nil,
+	}
+
+	addr := connData.GetIdentifier()
+	assert.EqualValues(t, "hello", addr)
 }
